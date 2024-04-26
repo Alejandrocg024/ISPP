86bcf2280ef49b1da5386318f1cb13d39939a7e1
Juan Jesús Campos Garrido <72499336+jucamgar2@users.noreply.github.com>
2024-04-26 15:28:08 +0200

    Update CustomDesignPrinters.jsx

--- frontend/src/components/Design/CustomDesignPrinters.jsx
+++ frontend/src/components/Design/CustomDesignPrinters.jsx
@@ -181,7 +181,7 @@ const CustomDesignPrinters = () => {
                 </>
               )}
             </div>
-            {data.status === "printing" && (
+            {data && currentUser && data.status === "printing" && currentUser.id != data.buyer && (
               <div style={{ textAlign: "center", alignItems: "center" }}>
                 {data.status === "printing" && (
                   <button className="large-btn button" onClick={handleFinish}>
